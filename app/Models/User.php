482e18e93c90cc292643d654736d474f1df422cb
JulieValdes <julieworks2@hotmail.com>
2025-03-08 23:07:33 -0600

    code_expires_at

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'verification_code',
+        'verification_code_expires_at',
     ];
 
     /**
